a50cd60826e1d0a47c78de2b6d05a9ae3c0af8e0
srishjaiz <srishjaiz@gmail.com>
2021-06-15 01:06:14 +0530

    favorite context file created

---
+++ src/store/favorites-context.js
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+
+const FavoriteContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  isItemFavorite: (meetupId) => {},
+});
+
+function FavoriteContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+  function isItemFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    isItemFavorite: isItemFavoriteHandler,
+  };
+
+  return (
+    <FavoriteContext.Provider value={context}>
+      {props.children}
+    </FavoriteContext.Provider>
+  );
+}
